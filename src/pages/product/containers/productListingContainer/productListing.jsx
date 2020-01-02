bb630e55d29f2d57905b8c176e13cd61934a921f
Asad Manzoor <asad33manzoor@gmail.com>
2020-01-02 16:47:21 +0500

    Hide default products

--- src/pages/product/containers/productListingContainer/productListing.jsx
+++ src/pages/product/containers/productListingContainer/productListing.jsx
@@ -11,100 +11,104 @@ import { productActions } from './actions';
 const ProductListingContainer = (props) => {
   const { products_listing, user_id, puchased_products_ids } = props;
   const products = products_listing.map((product, index) => {
-    let purchased = puchased_products_ids.indexOf(product.Record.id) > -1;
-    return (
-      <div key={index} className="col-xs-12 col-sm-6 col-md-4">
-        <div className="image-flip">
-          <div className="mainflip">
-            <div className="frontside">
-              <div className="card">
-                <div className="card-body text-center">
-                  <p>
-                    <img className=" img-fluid" src={product.Record.image} alt="product" />
-                  </p>
-                  <h4 className="card-title">{product.name}</h4>
-                  <p className="card-text">{product.Record.description}</p>
-                  <p className="card-text">
-                    <strong>Product Type</strong>: {product.Record.docType}
-                    <br />
-                    <strong>Price</strong>: {product.Record.price}
-                    <br />
-                    <strong>Quantity</strong>: {product.Record.quantity}
-                    <br />
-                  </p>
+    if (product.Key !== 'ID0' && product.Key !== 'ID1') {
+      let purchased = puchased_products_ids.indexOf(product.Record.id) > -1;
+      return (
+        <div key={index} className="col-xs-12 col-sm-6 col-md-4">
+          <div className="image-flip">
+            <div className="mainflip">
+              <div className="frontside">
+                <div className="card">
+                  <div className="card-body text-center">
+                    <p>
+                      <img className=" img-fluid" src={product.Record.image} alt="product" />
+                    </p>
+                    <h4 className="card-title">{product.Record.name}</h4>
+                    <p className="card-text">{product.Record.description}</p>
+                    <p className="card-text">
+                      <strong>Product Type</strong>: {product.Record.docType}
+                      <br />
+                      <strong>Price</strong>: {product.Record.price}
+                      <br />
+                      <strong>Quantity</strong>: {product.Record.quantity}
+                      <br />
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="mainflip backside">
-              <div className="card">
-                <div className="card-body text-center">
-                  <Link
-                    style={{ display: purchased ? '' : 'none' }}
-                    to="/review_product"
-                    className="btn btn-success btn-lg"
-                    onClick={() =>
-                      props.dispatch({
-                        type: updateProductConstants.EDIT_PRODUCT,
-                        id: product.Record.id,
-                        product: product
-                      })
-                    }
-                  >
-                    Review Product
-                  </Link>
-                  <br />
-                  <br />
-                  <span style={{ display: purchased ? '' : 'none' }} className="btn btn-success btn-lg">
-                    Already Purchased
-                  </span>
-                  <span
-                    style={{ display: purchased ? 'none' : '' }}
-                    className="btn btn-success btn-lg"
-                    onClick={() => props.dispatch(productActions.purchaseProduct(product.Record.id, user_id))}
-                  >
-                    Purchase Product
-                  </span>
-                  <br />
-                  <br />
-                  <Link
-                    to="/update_product"
-                    className="btn btn-success btn-lg"
-                    onClick={() =>
-                      props.dispatch({
-                        type: updateProductConstants.EDIT_PRODUCT,
-                        id: product.Record.id,
-                        product: product
-                      })
-                    }
-                  >
-                    Update Product
-                  </Link>
+              <div className="mainflip backside">
+                <div className="card">
+                  <div className="card-body text-center">
+                    <Link
+                      style={{ display: purchased ? '' : 'none' }}
+                      to="/review_product"
+                      className="btn btn-success btn-lg"
+                      onClick={() =>
+                        props.dispatch({
+                          type: updateProductConstants.EDIT_PRODUCT,
+                          id: product.Record.id,
+                          product: product
+                        })
+                      }
+                    >
+                      Review Product
+                    </Link>
+                    <br />
+                    <br />
+                    <span style={{ display: purchased ? '' : 'none' }} className="btn btn-success btn-lg">
+                      Already Purchased
+                    </span>
+                    <span
+                      style={{ display: purchased ? 'none' : '' }}
+                      className="btn btn-success btn-lg"
+                      onClick={() => props.dispatch(productActions.purchaseProduct(product.Record.id, user_id))}
+                    >
+                      Purchase Product
+                    </span>
+                    <br />
+                    <br />
+                    <Link
+                      to="/update_product"
+                      className="btn btn-success btn-lg"
+                      onClick={() =>
+                        props.dispatch({
+                          type: updateProductConstants.EDIT_PRODUCT,
+                          id: product.Record.id,
+                          product: product
+                        })
+                      }
+                    >
+                      Update Product
+                    </Link>
 
-                  <br />
-                  <br />
-                  <Link
-                    to="/detail_product"
-                    className="btn btn-success btn-lg"
-                    onClick={() =>
-                      props.dispatch({
-                        type: updateProductConstants.EDIT_PRODUCT,
-                        id: product.Record.id,
-                        product: product
-                      })
-                    }
-                  >
-                    Detail Information
-                  </Link>
-                  <h4 className="card-title">{product.name}</h4>
+                    <br />
+                    <br />
+                    <Link
+                      to="/detail_product"
+                      className="btn btn-success btn-lg"
+                      onClick={() =>
+                        props.dispatch({
+                          type: updateProductConstants.EDIT_PRODUCT,
+                          id: product.Record.id,
+                          product: product
+                        })
+                      }
+                    >
+                      Detail Information
+                    </Link>
+                    <h4 className="card-title">{product.name}</h4>
+                  </div>
                 </div>
               </div>
             </div>
+            <br />
+            <br />
           </div>
-          <br />
-          <br />
         </div>
-      </div>
-    );
+      );
+    } else {
+      return <div key={index}> </div>;
+    }
   });
 
   return (
